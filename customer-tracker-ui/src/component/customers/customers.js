2dddc1ce84f493883155a293fbf76b43acfb95a2
JesterCharles <JesterCharles@gmail.com>
2022-12-07 11:30:35 -0500

    showcased interval and timeouts, plus the variations of fetch then and async-await

--- customer-tracker-ui/src/component/customers/customers.js
+++ customer-tracker-ui/src/component/customers/customers.js
@@ -2,28 +2,72 @@ const url = "http://localhost:8080/"
 
 async function viewCustomers(){
 
+    intervalViewCustomers();
+
+    setInterval( async () => {
+        intervalViewCustomers()
+   
+}, 120000)
+}
+
+
+async function intervalViewCustomers(){
     try {
-    const response = await fetch(`${url}customer`,{
+        const response = await fetch(`${url}customer`,{
+            method: "GET",
+            headers: {
+                authorization: window.localStorage.getItem("token")
+            }
+        });
+    
+        const customers = await response.json();
+    
+        console.log(customers);
+        let html_code = "<tr>"
+        customers.map(element =>{
+            for(const key in element){
+                html_code += `<td>${element[key]}</td>`
+            }
+            html_code += "</tr>"
+            console.log(html_code)
+        })
+    
+        document.getElementById("customerTable").innerHTML = html_code
+    } catch(error){
+        console.error(error);
+    }
+}
+
+async function viewCustomersFTTC(){
+
+  fetch(`${url}customer`,{
         method: "GET",
         headers: {
             authorization: window.localStorage.getItem("token")
         }
-    });
-
-    const customers = await response.json();
-
-    console.log(customers);
-    let html_code = "<tr>"
-    customers.map(element =>{
-        for(const key in element){
-            html_code += `<td>${element[key]}</td>`
+    })
+    .then(response => {
+        if(response.status === 200){
+            setTimeout(() => alert("Woop Woop we did boisss"), 5000)
         }
-        html_code += "</tr>"
-        console.log(html_code)
+        return response.json()
+    })
+    .then(customers => {
+        console.log(customers);
+        let html_code = "<tr>"
+        customers.map(element =>{
+            for(const key in element){
+                html_code += `<td>${element[key]}</td>`
+            }
+            html_code += "</tr>"
+            console.log(html_code)
+         })
+
+        document.getElementById("customerTable").innerHTML = html_code
     })
 
-    document.getElementById("customerTable").innerHTML = html_code
-} catch(error){
-    console.error(error);
-}
+    
+    .catch(error => {
+        console.error(error);
+    })
 }
